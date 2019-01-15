a59222a21f3f0a6c0a10914eab4d230f3af27922
Youloookgood <1028317256@qq.com>
2019-01-15 14:30:35 +0800

    comments is alread.

--- src/modules/comment/index.vue
+++ src/modules/comment/index.vue
@@ -1,28 +1,30 @@
 <template>
     <section class="g-comment">
-        <operation :data="operation"/>
+        <operation @output="choice" :data="operation"/>
         <div>
-            <el-table :data="commentData">
-                <el-table-column label="评论者" prop="commenter">
+            <el-table :data="showData">
+                <el-table-column label="评论者" prop="name">
                     <template slot-scope="scope">
-                        <a href="javascript:;">{{scope.row.commenter}}</a>
+                        <a href="javascript:;">{{scope.row.name}}</a>
                     </template>
                 </el-table-column>
                 <el-table-column label="内容" prop="content">
-
                 </el-table-column>
                 <el-table-column label="评论页面">
                     <template slot-scope="scope">
-                        <a href="javascript:;">{{scope.row.page}}</a>
+                        <a href="javascript:;">暂无</a>
                     </template>
                 </el-table-column>
-                <el-table-column label="日期" prop="date">
+                <el-table-column label="日期">
 
                 </el-table-column>
                 <el-table-column label="操作">
-                    <template>
-                        <el-button type="primary" size="mini">回复</el-button>
-                        <el-button type="danger" size="mini">丢弃</el-button>
+                    <template slot-scope="opera">
+                        <el-button type="primary" size="mini" v-show="nowStatus == 0">回复</el-button>
+                        <el-button type="primary" size="mini" v-show="nowStatus == 1" @click="publish(opera.row)">发布</el-button>
+                        <el-button type="danger" size="mini" @click="abandon(opera.row)" v-show="nowStatus != 2">丢弃</el-button>
+                        <el-button type="primary" size="mini" v-show="nowStatus == 2" @click="restar(opera.row)">还原</el-button>
+                        <el-button type="danger" size="mini" v-show="nowStatus == 2" @click="deletes(opera.row)">彻底删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -43,11 +45,144 @@
                     {commenter: 'line', content:'welcome', page:'来啊', date:'2018-12-17', operate:''}
                 ],
                 operation:[
-                    {name:'已发布', number:1 },
-                    {name:'待审核', number:0 },
-                    {name:'回收站', number:0 },
+                    {name:'已发布', number:0, status:0 },
+                    {name:'待审核', number:0, status:1 },
+                    {name:'回收站', number:0, status:2 },
                 ],
+                nowStatus:0,
+                showData:[]
             }
+        },
+        methods:{
+            //彻底删除
+            deletes(row){
+               this.$http({
+                    url:'/apis/comments/operation',
+                    method:'post',
+                    data:{
+                        serial:row.serial,
+                        status:3
+                    }
+                }).then((res) => {
+                    this.operaitons(2,3);
+                    this.$message(res.data.message);
+                    
+                })  
+            },
+            //还原
+            restar(row){
+                this.$http({
+                    url:'/apis/comments/operation',
+                    method:'post',
+                    data:{
+                        serial:row.serial,
+                        status:0
+                    }
+                }).then((res) => {
+                    this.operaitons(this.nowStatus,0)
+                    this.$message(res.data.message);
+                    
+                })
+            },
+            //发布
+            publish(row){
+               this.$http({
+                    url:'/apis/comments/operation',
+                    method:'post',
+                    data:{
+                        serial:row.serial,
+                        status:0
+                    }
+                }).then((res) => {
+                    this.operaitons(this.nowStatus,0)
+                    this.$message(res.data.message);
+                }) 
+            },
+            //丢弃
+            abandon(row){
+                this.$http({
+                    url:'/apis/comments/operation',
+                    method:'post',
+                    data:{
+                        serial:row.serial,
+                        status:2
+                    }
+                }).then((res) => {
+                    this.operaitons(this.nowStatus,2)
+                    this.$message(res.data.message);
+                    
+                })
+            },
+            //选择
+            choice(status){
+                switch(status){
+                    case 0:
+                        this.nowStatus = 0;
+                        this.operaitons(0);
+                        break;
+                    case 1:
+                        this.nowStatus = 1;
+                        this.operaitons(1);
+                        break;
+                    case 2:
+                        this.nowStatus = 2;
+                        this.operaitons(2);
+                        break;        
+                }
+            },
+            operaitons(number,op){
+                this.$http({
+                    url:'/apis/comments/getComments',
+                    method:'get',
+                    params:{
+                        status:number
+                    }
+                }).then((res)=>{
+                    if(!op && op != 0){
+                        return  this.showData = res.data.data
+                    }
+                    if(op == 3){
+                        this.operation[this.nowStatus].number -= 1;
+                        return this.showData = res.data.data
+                    }
+                    this.operation[this.nowStatus].number -= 1;
+                    this.operation[op].number += 1;
+                    this.showData = res.data.data
+                })
+            },
+            init(){
+                return new Promise((resolve, reject)=>{
+                    this.$http({
+                        url:'/apis/comments/getComments',
+                        method:'get',
+                        params:{
+                            status:0,
+                            quantity:'all'
+                        }
+                    }).then((res)=>{
+                        res.data.datas.map((item)=>{
+                            if(item.status == 0){
+                                this.operation[0].number +=1
+                                this.showData.push(item)
+                            }else if(item.status ==1){
+                                this.operation[1].number +=1
+                            }else{
+                                this.operation[2].number +=1
+                            }
+                            
+                        })
+                        resolve(res)
+                    }).catch((err) =>{
+                        reject(err)
+                    })
+                })
+            },
+
+
+        },
+        mounted(){
+            //初始化
+            this.init()
         },
         components:{
             operation,
--- src/pages/home/bodyDetails/index.vue
+++ src/pages/home/bodyDetails/index.vue
@@ -14,6 +14,38 @@
                         <p><span>{{detailData.author}}</span>/<span>暂无类型</span>/<span>{{detailData.upTime | timeChange}}</span>/<span>暂无阅读量</span></p>
                     </div>    
                 </template>
+                <div class="comments f-cb">
+                    <div class="u-img f-fl">
+                        <img src="../../../assets/aisi.jpg" alt="评论图" class="u-img">
+                    </div>
+                    <div class="f-fl u-area">
+                        <p>
+                            <input type="text"  v-model="comments.name" placeholder="昵称(必填)">
+                            <input type="text" v-model="comments.email" placeholder="邮箱(选填)">
+                            <input type="text" v-model="comments.address" placeholder="网址(选填)">
+                        </p>
+                        <div class="f-cb">
+                            <textarea v-model="comments.text" placeholder="赶快评论一个吧">
+                            </textarea>
+                            <el-button @click="submit" size="mini" class="f-fr">提交</el-button>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-vistor">
+                    <p>{{showComments.length}}评论</p>
+                    <ul>
+                        <li class="f-cb" v-for="item in showComments" :key="item.serial">
+                            <div class="img f-fl">
+                                <img src="../../../assets/vistor.png" alt="游客">
+                            </div>
+                            <div class="f-fl">
+                                <p>{{item.name}}</p>
+                                <p>{{item.content}}</p>
+                                <p><span class="time">2019-01-11:43</span>  <el-button type="danger" size="mini" v-show="false">回复</el-button></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </article>
 
             <article class="f-fl artSize">
@@ -35,25 +67,82 @@
         data(){
             return {
                 detailData:{title:'', author:'', upTime:''},
+                comments:{
+                    name:'',
+                    email:'',
+                    address:'',
+                    text:'',
+                    relation:1
+                },
+                showComments:[]
             }
+        },
+        computed:{
+           endComments(){
+               return {
+                   name: this.comments.name,
+                   email: this.comments.email,
+                   address: this.comments.address,
+                   text: this.comments.text,
+                   relation: this.comments.relation
+               }
+           } 
         },
         methods:{
             init(){
-                this.$http({
+              return  new Promise((resolve, reject)=>{
+                  this.$http({
                     url:'/apis/article/getArticle',
                     method:'get',
                     params:{
                         serial:Number(this.$route.params.id),
                         page:'detail'
                     }
-                }).then(async (res) => {
-                    this.detailData = res.data.data;
-                    this.$refs.textContent.innerHTML = this.detailData.content;
+                    }).then(async (res) => {
+                        this.detailData = res.data.data;
+                        this.comments.relation = this.detailData.serial;
+                        this.$refs.textContent.innerHTML = this.detailData.content;
+                        resolve(this.comments.relation)
+                    }).catch((err)=>{
+                        reject(err)
+                    })
+                })
+                
+            },
+            submit(){
+                if(!this.comments.name){
+                    this.$message('评论时用户名称必须填哦')
+                    return 
+                }
+                this.$http({
+                    url:'/apis/comments/createOroperation',
+                    method:'post',
+                    data:{
+                        content:this.endComments,
+                        status:1
+                    }
+                }).then((res) => {
+                    this.getComment()
+                    this.$message(res.data.message)
+                })
+            },
+            getComment(){
+                this.$http({
+                    url:'/apis/comments/getArticleComment',
+                    method:'get',
+                    params:{
+                        relation:this.comments.relation
+                    }
+                }).then((res) => {
+                    this.showComments = res.data.data
                 })
             }
         },
         mounted(){
-            this.init()
+            this.init().then((res)=>{
+                console.log('promise', res)
+                this.getComment()
+            })
         },
         created(){
         }
@@ -67,6 +156,81 @@
             width: 80%;
             border-right: 1px solid #ccc;
             min-height: 1000px;
+            .comments{
+                width: 1100px;
+                margin: 20px auto;
+                .u-img{
+                    width: 60px;
+                    height: 60px;
+                    border-radius: 50%;
+                    margin-right: 20px;
+                }
+                .u-area{
+                    margin-bottom: 20px;
+                    box-sizing: border-box;
+                    padding: 10px;
+                    border: 1px solid #f4f4f4;
+                    min-height: 265px;
+                    min-width: 950px;
+                    p{
+                        input{
+                            padding: 10px 0;
+                            margin-right: 4px;
+                            border-left: none;
+                            border-top: none;
+                            border-right: none;
+                            width: 300px;
+                            border-bottom: 1px dashed #dedede;
+                            font-size: 14px;
+                            &:focus{
+                                border-bottom: 1px dashed red;
+                            }
+                        }
+                    }
+                    div{
+                        margin-top: 20px;
+                        textarea{
+                            width: 100%;
+                            min-height: 200px;
+                            font-size: 16px;
+                            border: none;
+                        }
+                    }
+                }
+
+            }
+            .m-vistor{
+                margin-left: 140px;
+                img{
+                    width: 60px;
+                    height: 60px;
+                    border-radius: 50%;
+                    margin-right: 20px; 
+                }
+                p{
+                    margin-bottom: 8px;
+                    min-width: 900px;
+                    font-size: 14px;
+                }
+                div{
+                    p:nth-of-type(1){
+                        font-size: 16px;
+                    }
+                    p:nth-of-type(2){
+                        margin: 18px 0;
+                    }
+                    p:nth-of-type(3){
+                        padding-bottom: 20px;
+                        border-bottom: 1px solid #f5f5f5;
+                        .time{
+                            color: #b3b3b3;
+                        }
+                    }
+                }
+                li{
+                    margin-bottom: 10px;
+                }
+            }
             .typeTitle{
                  margin-bottom: 20px;
                  text-align: center;
@@ -75,7 +239,7 @@
                 box-sizing: content-box;
                 margin: 20px;
                 background: #f5f5f5;
-                min-height: 300px;
+                min-height: 800px;
                 padding: 20px 10px;
                 box-shadow: -4px 7px 46px 2px rgba(0,0,0,0.1);
             }   
